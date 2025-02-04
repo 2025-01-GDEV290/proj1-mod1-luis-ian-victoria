16fca52107b8e6489f75483885a682aa4e07e9da
IanMancuso <Ian.mancuso@gmail.com>
2025-02-04 14:48:48 -0500

    Scenes combined, explosion added

--- Assets/__Scripts/Wall.cs
+++ Assets/__Scripts/Wall.cs
@@ -7,7 +7,14 @@ public class Wall : MonoBehaviour
 
     private int count;
     private float velocity;
+    public float radius = 100.0F;
+    public float power = 10.0F;
     public Rigidbody rb;
+    public GameObject door1;
+    public GameObject door2;
+    public GameObject door3;
+    public GameObject door4;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -31,10 +38,39 @@ public class Wall : MonoBehaviour
 
             if(velocity >= 10) {
                 count++;
-                if(count >= 3)
+
+                switch(count)
                 {
-                    gameObject.SetActive(false);
+                    case 1:
+                        door1.SetActive(false);
+                        door2.SetActive(true);
+                        break;
+                    case 2:
+                        door2.SetActive(false);
+                        door3.SetActive(true);
+                        break;
+                    case 3:
+                        door3.SetActive(false);
+                        door4.SetActive(true);
+                        Vector3 explosionPos = transform.position - new Vector3(0,0,10);
+                        Collider[] colliders = Physics.OverlapSphere(explosionPos, radius);
+                        foreach (Collider hit in colliders)
+                        {
+                            Debug.Log(hit.gameObject.name);
+                            Rigidbody rb = hit.GetComponent<Rigidbody>();
+
+                            if (rb != null)
+                                rb.AddExplosionForce(power, explosionPos, radius, 0);
+                        }
+                        gameObject.SetActive(false);
+                        break;
+                    default:
+                        break;
                 }
+                //if(count >= 3)
+                //{
+                //   gameObject.SetActive(false);
+                //}
             }
 
         }
